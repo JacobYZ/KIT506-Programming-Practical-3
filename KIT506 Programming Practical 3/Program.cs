fb518913851656e9f8438d9791dec66981afc6de
Yu Zhang <zhangy51@utas.edu.au>
2020-08-05 21:25:35 +1000

    Add project files.

---
+++ KIT506 Programming Practical 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace KIT506_Programming_Practical_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] counts = { 1, 0, 5, 7, 4, 2, 1 };
+            for (int i = 0; i < counts.Length; i++)
+            {
+                Console.WriteLine(i + " | " + counts[i]);
+            }
+            for (int i = 0; i < counts.Length; i++)
+            {
+                Console.Write(i + " | ");
+                for(int j = 0; j < counts[i]; j++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
